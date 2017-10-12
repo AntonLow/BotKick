89244067d5cfd64c3fce363daa93775f2a05d5f7
AntonLow <32382974+AntonLow@users.noreply.github.com>
2017-10-12 12:09:51 +0700

    Update LineClient.py

--- LineAlpha/LineApi/LineClient.py
+++ LineAlpha/LineApi/LineClient.py
@@ -31,7 +31,7 @@ class LineClient(LineApi):
     @loggedIn
     def _loginresult(self):
         if self.isLogin == True:
-            print "\n\nBotKick\nMade by Farzain - zFz\nKalau mau di-edit, silahkan\nini juga pake LineVodka dari Merkremont\n"
+            print "\n\nBotKick\nMade by AntonLow\nKalau mau di-edit, silahkan\nini juga pake LineVodka dari Merkremont\n"
             print "authToken : " + self.authToken + "\n"
             print "certificate : " + self.certificate + "\n"
             """:type profile: Profile"""
